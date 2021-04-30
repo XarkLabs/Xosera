63e8beb33bb231e1ce79fed2d264f77bae37f8bf
Xark <Xark@XarkLabs.com>
2021-04-30 03:09:30 -0700

    rosco_m68k optimized low-level C API

---
+++ xosera_test_m68k/xosera_regs.h
@@ -0,0 +1,83 @@
+/*
+ * vim: set et ts=4 sw=4
+ *------------------------------------------------------------
+ *                                  ___ ___ _
+ *  ___ ___ ___ ___ ___       _____|  _| . | |_
+ * |  _| . |_ -|  _| . |     |     | . | . | '_|
+ * |_| |___|___|___|___|_____|_|_|_|___|___|_,_|
+ *                     |_____|
+ * ------------------------------------------------------------
+ * Copyright (c) 2021 Xark
+ * MIT License
+ *
+ * Xosera low-level C API to read/write Xosera registers
+ * ------------------------------------------------------------
+ */
+
+#include <stdint.h>
+
+// NOTE: Since Xosera is using a 6800-style 8-bit bus, it uses only data lines 8-15 of each 16-bit word
+//       (i.e., only the upper byte of each word) this makes the size of its register map in memory
+//       appear doubled and is the reason for the pad bytes in the struct below.  Byte access is fine
+//       but for word or long access to this struct, the MOVEP 680x0 instruction should be used (it
+//       was designed for this purpose).  The xv-set* macros below make it easy.
+typedef struct
+{
+    union
+    {
+        struct
+        {
+            volatile uint8_t h;
+            volatile uint8_t _h_pad;
+            volatile uint8_t l;
+            volatile uint8_t _l_pad;
+        } b;
+        const volatile uint16_t w;    // NOTE: For use as offset only with xv_setw (and MOVEP.W opcode)
+        const volatile uint32_t l;    // NOTE: For use as offset only with xv_setl (and MOVEP.L opcode)
+    };
+} xreg_t;
+
+// Xosera register map (See https://github.com/XarkLabs/Xosera/blob/develop/REFERENCE.md)
+typedef struct
+{
+    xreg_t aux_addr;    // address for AUX read/write via AUX_DATA (see below)
+    xreg_t const_val;   // set constant value (e.g. for VRAM fill) TODO
+    xreg_t rd_addr;     // set read address for reading from VRAM via DATA/DATA_2
+    xreg_t wr_addr;     // set write address for writing to VRAM
+    xreg_t data;        // read/write VRAM word at RD/WR_ADDR then add RD/WR_INC
+    xreg_t data_2;      // 2nd DATA (to allow for 32-bit read/write to VRAM)
+    xreg_t aux_data;    // read/write AUX word at AUX_ADDR
+    xreg_t count;       // write blitter count and start operation or read blitter status TODO
+    xreg_t rd_inc;      // RD_ADDR increment per word read
+    xreg_t wr_inc;      // WR_ADDR increment per word write
+    xreg_t wr_mod;      // WR_ADDR increment every `WIDTH` words read (in 2D mode) TODO
+    xreg_t rd_mod;      // RD_ADDR increment every `WIDTH` words read (in 2D mode) TODO
+    xreg_t width;       // width in words for 2D rectangular blit
+    xreg_t blit_ctrl;   // set blitter and other options (reconfigure mode)
+    xreg_t unused_E;    // T.B.D. TODO
+    xreg_t unused_F;    // T.B.D. TODO
+}  xosera_regs_t;
+
+ // Xosera register base
+extern xosera_regs_t * xosera_ptr;
+#define xv_setbh(xr, v) do { xosera_ptr->xr.b.h = (v); } while (0)
+#define xv_setbl(xr, v) do { xosera_ptr->xr.b.l = (v); } while (0)
+#define xv_setw(xr, v) do { __asm__ __volatile__ ("movep.w %[src],%[dst]" : : [src] "d" ((v)), [dst] "m" (xosera_ptr->xr) : ); } while (0)
+#define xv_setl(xr, v) do { __asm__ __volatile__ ("movep.l %[src],%[dst]" : : [src] "d" ((v)), [dst] "m" (xosera_ptr->xr) : ); } while (0)
+
+#define xv_aux_setbh(xa, v) do { xv_setw(aux_addr, (xa)); xosera_ptr->aux_data.b.h = (v); } while (0)
+#define xv_aux_setbl(xa, v) do { xv_setw(aux_addr, (xa)); xosera_ptr->aux_data.b.l = (v); } while (0)
+#define xv_aux_setw(xa, v) do { xv_setw(aux_addr, (xa)); __asm__ __volatile__ ("movep.w %[src],%[dst]" : : [src] "d" ((v)), [dst] "m" (xosera_ptr->aux_data) : ); } while (0)
+#define xv_aux_setl(xa, v) do { xv_setw(aux_addr, (xa)); __asm__ __volatile__ ("movep.l %[src],%[dst]" : : [src] "d" ((v)), [dst] "m" (xosera_ptr->aux_data) : ); } while (0)
+
+// NOTE: Uses clang and gcc supported extension (statement expression), so we must slightly lower shields...
+#pragma GCC diagnostic ignored "-Wpedantic"     // Yes, I'm cheating (but ugly to have to pass in "return variable" - and this is the "low level" API, remember)
+
+#define xv_getbh(xr) (xosera_ptr->xr.b.h)
+#define xv_getbl(xr) (xosera_ptr->xr.b.l)
+#define xv_getw(xr) ({ uint16_t w; __asm__ __volatile__ ("movep.w %[src],%[dst]" : [dst] "=d" (w) : [src] "m" (xosera_ptr->xr): ); w; })
+#define xv_getl(xr) ({ uint32_t l; __asm__ __volatile__ ("movep.l %[src],%[dst]" : [dst] "=d" (l) : [src] "m" (xosera_ptr->xr): ); l; })
+
+#define xv_aux_getbh(xa) do ( xv_setw(aux_addr, (xa)), xosera_ptr->aux_data.b.h)
+#define xv_aux_getbl(xa) do ( xv_setw(aux_addr, (xa)), xosera_ptr->aux_data.b.l)
+#define xv_aux_getw(xa) ({ uint16_t w; xv_setw(aux_addr, (xa)); __asm__ __volatile__ ("movep.w %[src],%[dst]" : [dst] "=d" (w) : [src] "m" (xosera_ptr->aux_data): ); w; })
---
+++ xosera_test_m68k/xosera_test_m68k.c
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <basicio.h>
+#include <debug_stub.h>
+#include <machine.h>
+
+#include "xosera_regs.h"
+
+// rosco_m68k Xosera board address (See https://github.com/rosco-m68k/hardware-projects/blob/feature/xosera/xosera/code/pld/decoder/ic3_decoder.pld#L25)
+xosera_regs_t * xosera_ptr = (xosera_regs_t *)0xf80060;
+
+void kmain()
+{
+    debug_stub();
+    mcDelaymsec10(300);
+
+    printf("Hello from rosco_m68k, now go look at the other monitor. :)\n");
+
+    xv_setw(wr_addr, 0x0000);
+    xv_setw(wr_inc, 1);
+    xv_setw(data, 0x0200 | 'X');
+    xv_setbl(data, 'o');
+    xv_setbl(data, 's');
+    xv_setbl(data, 'e');
+    xv_setbl(data, 'r');
+    xv_setbl(data, 'a');
+    xv_setbl(data, ' ');
+    xv_setw(data, 0x0400 | '6');
+    xv_setbl(data, '8');
+    xv_setbl(data, 'k');
+
+    // read test
+    xv_setw(rd_addr, 0x0000);
+    uint16_t val = xv_getw(data);
+    printf("Read back = %04x\nBye!\n", val);
+
+    return;
+}

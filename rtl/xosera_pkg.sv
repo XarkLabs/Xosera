0d90293ad2ec5f8f8ff6b9879b31189bd1c78c92
Daniel Cliche <dcliche@meldora.com>
2021-08-06 21:27:46 -0400

    Added video mode 640x480 75Hz

--- rtl/xosera_pkg.sv
+++ rtl/xosera_pkg.sv
@@ -80,6 +80,7 @@ typedef enum logic [15:0]{
 
 `ifdef MODE_640x400     // 25.175 MHz (requested), 25.125 MHz (achieved)
 `elsif MODE_640x480     // 25.175 MHz (requested), 25.125 MHz (achieved)
+`elsif MODE_640x480_75  // 31.500 MHz (requested), 31.500 MHz (achieved)
 `elsif MODE_720x400     // 28.322 MHz (requested), 28.500 MHz (achieved)
 `elsif MODE_848x480     // 33.750 MHz (requested), 33.750 MHz (achieved)
 `elsif MODE_800x600     // 40.000 MHz (requested), 39.750 MHz (achieved) [tight timing]
@@ -90,7 +91,7 @@ typedef enum logic [15:0]{
 `endif
 
 `ifdef    MODE_640x400
-// VGA mode 640x480 @ 60Hz (pixel clock 25.175Mhz)
+// VGA mode 640x400 @ 70Hz (pixel clock 25.175Mhz)
 localparam PIXEL_FREQ        = 25_175_000;  // pixel clock in Hz
 localparam VISIBLE_WIDTH     = 640;         // horizontal active pixels
 localparam VISIBLE_HEIGHT    = 400;         // vertical active lines
@@ -114,6 +115,20 @@ localparam H_BACK_PORCH      = 48;          // H post-sync (back porch) pixels
 localparam V_FRONT_PORCH     = 10;          // V pre-sync (front porch) lines
 localparam V_SYNC_PULSE      = 2;           // V sync pulse lines
 localparam V_BACK_PORCH      = 33;          // V post-sync (back porch) lines
+localparam H_SYNC_POLARITY   = 1'b0;        // H sync pulse active level
+localparam V_SYNC_POLARITY   = 1'b0;        // V sync pulse active level
+
+`elsif    MODE_640x480_75
+// VGA mode 640x480 @ 75Hz (pixel clock 31.500Mhz)
+localparam PIXEL_FREQ        = 31_500_000;  // pixel clock in Hz
+localparam VISIBLE_WIDTH     = 640;         // horizontal active pixels
+localparam VISIBLE_HEIGHT    = 480;         // vertical active lines
+localparam H_FRONT_PORCH     = 16;          // H pre-sync (front porch) pixels
+localparam H_SYNC_PULSE      = 64;          // H sync pulse pixels
+localparam H_BACK_PORCH      = 120;         // H post-sync (back porch) pixels
+localparam V_FRONT_PORCH     = 1;           // V pre-sync (front porch) lines
+localparam V_SYNC_PULSE      = 3;           // V sync pulse lines
+localparam V_BACK_PORCH      = 16;          // V post-sync (back porch) lines
 localparam H_SYNC_POLARITY   = 1'b0;        // H sync pulse active level
 localparam V_SYNC_POLARITY   = 1'b0;        // V sync pulse active level
 
@@ -216,6 +231,10 @@ localparam PLL_DIVQ    =    3'b101;         // DIVQ =  5
 localparam PLL_DIVR    =    4'b0000;        // DIVR =  0
 localparam PLL_DIVF    =    7'b1000010;     // DIVF = 66
 localparam PLL_DIVQ    =    3'b101;         // DIVQ =  5
+`elsif    MODE_640x480_75 // 31.500 MHz (requested), 31.500 MHz (achieved)
+localparam PLL_DIVR    =    4'b0000;        // DIVR =  0
+localparam PLL_DIVF    =    7'b1010011;     // DIVF = 83
+localparam PLL_DIVQ    =    3'b101;         // DIVQ =  5
 `elsif    MODE_720x400  // 28.322 MHz (requested), 28.500 MHz (achieved)
 localparam PLL_DIVR    =    4'b0000;        // DIVR =  0
 localparam PLL_DIVF    =    7'b1001011;     // DIVF = 75
@@ -239,7 +258,7 @@ localparam PLL_DIVQ    =    3'b011;         // DIVQ =  3
 `endif
 `endif
 
-//`define TESTPATTERN     // init with "test pattern" instead of clear VRAM
+`define TESTPATTERN     // init with "test pattern" instead of clear VRAM
 
 endpackage
 `endif

598187cc954d0db5faf902963a01e01e049017db
Xark <Xark@XarkLabs.com>
2021-09-10 17:34:00 -0700

    ANSI color test script

---
+++ xosera_ansiterm_m68k/colors_and_formatting.sh
@@ -0,0 +1,26 @@
+    #!/bin/bash
+     
+    # This program is free software. It comes without any warranty, to
+    # the extent permitted by applicable law. You can redistribute it
+    # and/or modify it under the terms of the Do What The Fuck You Want
+    # To Public License, Version 2, as published by Sam Hocevar. See
+    # http://sam.zoy.org/wtfpl/COPYING for more details.
+
+    # NOTE: From <https://misc.flogisoft.com/bash/tip_colors_and_formatting>
+    #       Modified slightly for macOS (using 'gecho' from 'brew install coreutils')
+    #      - Xark
+
+    #Background
+    for clbg in {40..47} {100..107} 49 ; do
+    	#Foreground
+    	for clfg in {30..37} {90..97} 39 ; do
+    		#Formatting
+    		for attr in 0 1 2 4 5 7 ; do
+    			#Print the result
+    			gecho -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
+    		done
+    		echo #Newline
+    	done
+    done
+     
+    exit 0

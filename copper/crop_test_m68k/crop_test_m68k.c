e6f086a4143ff8220c4c8ebcd5826560b7bc73fc
Xark <Xark@XarkLabs.com>
2021-09-15 10:59:26 -0700

    Add wait for key to prevent print corruption

--- copper/crop_test_m68k/crop_test_m68k.c
+++ copper/crop_test_m68k/crop_test_m68k.c
@@ -32,21 +32,27 @@
 
 // Copper list
 const uint8_t  copper_list_len = 10;
-const uint16_t copper_list[] = {
-    0x0028, 0x0002, // wait  0, 40                   ; Wait for line 40, H position ignored
-    0x9010, 0x0075, // mover 0x0075, PA_GFX_CTRL     ; Set to 8-bpp + Hx2 + Vx2
-    0x01b8, 0x0002, // wait  0, 440                  ; Wait for line 440, H position ignored
-    0x9010, 0x0080, // mover 0x0080, PA_GFX_CTRL     ; Blank
-    0x0000, 0x0003  // nextf
+const uint16_t copper_list[]   = {
+    0x0028,
+    0x0002,        // wait  0, 40                   ; Wait for line 40, H position ignored
+    0x9010,
+    0x0075,        // mover 0x0075, PA_GFX_CTRL     ; Set to 8-bpp + Hx2 + Vx2
+    0x01b8,
+    0x0002,        // wait  0, 440                  ; Wait for line 440, H position ignored
+    0x9010,
+    0x0080,        // mover 0x0080, PA_GFX_CTRL     ; Blank
+    0x0000,
+    0x0003        // nextf
 };
 
-void     xosera_test()
+void xosera_test()
 {
     xosera_init(0);
-    
+
     xm_setw(XR_ADDR, XR_COPPER_MEM);
 
-    for (uint8_t i = 0; i < copper_list_len; i++) {
+    for (uint8_t i = 0; i < copper_list_len; i++)
+    {
         xm_setw(XR_DATA, copper_list[i]);
     }
 
@@ -60,5 +66,8 @@ void     xosera_test()
             xm_setw(DATA, x == 0 || y == 0 || x == (320 / 2 - 1) || y == (200 - 1) ? 0x0f0f : 0x0101);
 
     // enable Copper
-    xreg_setw(COPP_CTRL, 0x8000);                  
+    xreg_setw(COPP_CTRL, 0x8000);
+
+    // wait for a key (so prints don't mess up screen)
+    readchar();
 }

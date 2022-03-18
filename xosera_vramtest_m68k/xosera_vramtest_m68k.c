12bc5d2e59ec1537b93fc2ef8279aca3807d7acc
Xark <Xark@XarkLabs.com>
2022-03-17 23:14:36 -0700

    add test TILEMEM, COLORMEM and COPPERMEM also

--- xosera_vramtest_m68k/xosera_vramtest_m68k.c
+++ xosera_vramtest_m68k/xosera_vramtest_m68k.c
@@ -92,23 +92,24 @@ uint16_t vram_buffer[64 * 1024];           // buffer to hold copy of VRAM data
 enum vram_test_flags
 {
     // test type
-    MODEFLAG_SLOW = (1 << 0),
-    MODEFLAG_BYTE = (1 << 1),
-    MODEFLAG_WORD = (1 << 2),
-    MODEFLAG_LONG = (1 << 3),
+    MODEFLAG_SLOW  = (1 << 0),
+    MODEFLAG_BYTE  = (1 << 1),
+    MODEFLAG_WORD  = (1 << 2),
+    MODEFLAG_LONG  = (1 << 3),
+    MODEFLAG_XRMEM = (1 << 4),
     // test pattern
-    MODEFLAG_LFSR = (1 << 4),
-    MODEFLAG_ADDR = (1 << 5),
+    MODEFLAG_LFSR = (1 << 5),
+    MODEFLAG_ADDR = (1 << 6),
     // error severities
-    MODEFLAG_BAD   = (1 << 6),
-    MODEFLAG_WRITE = (1 << 7),
-    MODEFLAG_READ  = (1 << 8),
+    MODEFLAG_BAD   = (1 << 7),
+    MODEFLAG_WRITE = (1 << 8),
+    MODEFLAG_READ  = (1 << 9),
     // video modes
-    MODEFLAG_1BPP  = (1 << 9),
-    MODEFLAG_4BPP  = (1 << 10),
-    MODEFLAG_8BPP  = (1 << 11),
-    MODEFLAG_XBPP  = (1 << 12),
-    MODEFLAG_BLANK = (1 << 13)
+    MODEFLAG_1BPP  = (1 << 10),
+    MODEFLAG_4BPP  = (1 << 11),
+    MODEFLAG_8BPP  = (1 << 12),
+    MODEFLAG_XBPP  = (1 << 13),
+    MODEFLAG_BLANK = (1 << 14)
 };
 
 // error summary info
@@ -125,11 +126,11 @@ struct vram_fail_info
 #define MAX_ERROR_LOG 4096
 #define MAX_TEST_FAIL 16
 #define TEST_MODES    5
-#define TEST_SPEEDS   4
+#define TEST_SPEEDS   5
 int                   num_vram_fails;
 struct vram_fail_info vram_fails[MAX_ERROR_LOG];
 const char *          vram_mode_names[TEST_MODES] = {"1-BPP", "4-BPP", "8-BPP", "X-BPP", "blank"};
-const char *          speed_names[TEST_SPEEDS]    = {"SLOW", "BYTE", "WORD", "LONG"};
+const char *          speed_names[TEST_SPEEDS]    = {"SLOW", "BYTE", "WORD", "LONG", "XMEM"};
 const uint16_t        vram_modes[TEST_MODES]      = {0x0040, 0x0050, 0x0060, 0x0070, 0x0080};
 const uint16_t        vram_mode_flags[TEST_MODES] = {MODEFLAG_1BPP,
                                               MODEFLAG_4BPP,
@@ -402,9 +403,10 @@ int test_vram(bool LFSR, int mode, int speed)
     // set funky mode to show VRAM
     wait_vsync();
     xreg_setw(VID_CTRL, 0x0000);
+    xreg_setw(PA_LINE_LEN, 136);        // ~65536/480 words per line
     xreg_setw(PA_DISP_ADDR, 0x0000);
+    xreg_setw(PA_TILE_CTRL, 0x000F);                 // text mode
     xreg_setw(PA_GFX_CTRL, vram_modes[mode]);        // bitmap + 8-bpp Hx2 Vx1
-    xreg_setw(PA_LINE_LEN, 136);                     // ~65536/480 words per line
 
     dprintf("  > VRAM test=%s speed=%s mode=%s : ", LFSR ? "LFSR" : "ADDR", speed_names[speed], vram_mode_names[mode]);
 
@@ -573,6 +575,242 @@ int test_vram(bool LFSR, int mode, int speed)
     return vram_errs;
 }
 
+#define XR_TILEMAP (XR_TILE_ADDR + 0x1000)
+#define XR_COLS    28
+#define XR_ROWS    20
+
+static int xmem_retry(uint16_t addr, uint16_t baddata, bool LFSR, int mode)
+{
+    int retries = 0;
+    int rc      = 0;
+    // see if slow read retry will read it correctly (if not, assume
+    // it was a write error)
+    uint16_t data = (uint16_t)~pattern_buffer[addr];
+    while (++retries < 10)
+    {
+        data = xmem_getw_wait(addr);
+        if (data == pattern_buffer[addr])
+        {
+            add_fail(addr,
+                     baddata,
+                     pattern_buffer[addr],
+                     MODEFLAG_READ | (LFSR ? MODEFLAG_LFSR : MODEFLAG_ADDR) | vram_mode_flags[mode] | MODEFLAG_XRMEM);
+            rc = 0;        // read error
+            break;
+        }
+    }
+
+    // try to correct VRAM
+    if (data != pattern_buffer[addr])
+    {
+        for (retries = 0; retries < 10; retries++)
+        {
+            xmem_setw_wait(addr, pattern_buffer[addr]);
+            data = xmem_getw_wait(addr);
+            if (data == pattern_buffer[addr])
+            {
+                add_fail(addr,
+                         baddata,
+                         pattern_buffer[addr],
+                         MODEFLAG_WRITE | (LFSR ? MODEFLAG_LFSR : MODEFLAG_ADDR) | (vram_mode_flags[mode]) |
+                             MODEFLAG_XRMEM);
+                rc = 1;        // correctable write error
+                break;
+            }
+        }
+    }
+
+    // if 10 tries fail, mark it as uncorrectable
+    if (data != pattern_buffer[addr])
+    {
+        add_fail(addr,
+                 baddata,
+                 pattern_buffer[addr],
+                 MODEFLAG_BAD | (LFSR ? MODEFLAG_LFSR : MODEFLAG_ADDR) | (vram_mode_flags[mode]) | MODEFLAG_XRMEM);
+        rc = -1;        // uncorrectable error
+    }
+    else
+    {
+        vram_buffer[addr] = data;
+    }
+
+    // log error
+    vram_test_fail_count++;
+    if (first_failure)
+    {
+        dprintf("FAILED!\n");
+        first_failure = false;
+    }
+    dprintf("*** MISMATCH %s %s %s: XMEM[0x%04x]=0x%04x vs data[0x%04x]=0x%04x [Error #%d]\n",
+            LFSR ? "LFSR" : "ADDR",
+            speed_names[4],
+            rc < 0   ? "BAD! "
+            : rc > 0 ? "WRITE"
+                     : "READ ",
+            addr,
+            baddata,
+            addr,
+            pattern_buffer[addr],
+            vram_test_fail_count);
+
+    return rc;
+}
+
+static int verify_xmem(bool LFSR, int mode)
+{
+    int xmem_errs = 0;
+
+    // read XMEM back into vram_buffer
+    for (int addr = XR_COLOR_A_ADDR; addr < (XR_COLOR_A_ADDR + XR_COLOR_A_SIZE + XR_COLOR_A_SIZE); addr++)
+    {
+        uint16_t data = vram_buffer[addr];
+        if (data != pattern_buffer[addr])
+        {
+            xmem_retry((uint16_t)addr, data, LFSR, mode);
+            if (++xmem_errs >= MAX_TEST_FAIL)
+            {
+                return xmem_errs;
+            }
+        }
+    }
+    for (int addr = XR_TILE_ADDR; addr < (XR_TILE_ADDR + XR_TILE_SIZE); addr++)
+    {
+        uint16_t data = vram_buffer[addr];
+        if (data != pattern_buffer[addr])
+        {
+            xmem_retry((uint16_t)addr, data, LFSR, mode);
+            if (++xmem_errs >= MAX_TEST_FAIL)
+            {
+                return xmem_errs;
+            }
+        }
+    }
+    for (int addr = XR_COPPER_ADDR; addr < (XR_COPPER_ADDR + XR_COPPER_SIZE); addr++)
+    {
+        uint16_t data = vram_buffer[addr];
+        if (data != pattern_buffer[addr])
+        {
+            xmem_retry((uint16_t)addr, data, LFSR, mode);
+            if (++xmem_errs >= MAX_TEST_FAIL)
+            {
+                return xmem_errs;
+            }
+        }
+    }
+
+    return xmem_errs;
+}
+
+static void read_xmem_buffer()
+{
+    xv_prep();
+
+    // read XMEM back into vram_buffer
+    for (int addr = XR_COLOR_A_ADDR; addr < (XR_COLOR_A_ADDR + XR_COLOR_A_SIZE + XR_COLOR_A_SIZE); addr++)
+    {
+        uint16_t data     = xmem_getw_wait(addr);
+        vram_buffer[addr] = data;
+    }
+    for (int addr = XR_TILE_ADDR; addr < (XR_TILE_ADDR + XR_TILE_SIZE); addr++)
+    {
+        uint16_t data     = xmem_getw_wait(addr);
+        vram_buffer[addr] = data;
+    }
+    for (int addr = XR_COPPER_ADDR; addr < (XR_COPPER_ADDR + XR_COPPER_SIZE); addr++)
+    {
+        uint16_t data     = xmem_getw_wait(addr);
+        vram_buffer[addr] = data;
+    }
+}
+
+int test_xmem(bool LFSR, int mode)
+{
+    int xmem_errs = 0;
+    first_failure = true;
+    xv_prep();
+
+    // set funky mode to show XMEM
+    wait_vsync();
+
+    wait_vsync();
+    xreg_setw(PA_GFX_CTRL, 0x0080);
+    xm_setw(XR_ADDR, XR_TILEMAP);
+    for (int i = 0; i < (XR_COLS * XR_ROWS); i++)
+    {
+        xm_setw(XR_DATA, i);
+    }
+    wait_vsync();
+    xreg_setw(PA_GFX_CTRL, 0x000A | mode);        // colorbase = 0x00 tiled + 8-bpp + Hx3 + Vx2
+    xreg_setw(PA_TILE_CTRL, 0x0207);              // tile=0x0000,tile=tile_mem, map=tile_mem, 8x8 tiles
+    xreg_setw(PA_LINE_LEN, XR_COLS);
+    xreg_setw(PA_DISP_ADDR, XR_TILEMAP);
+
+    dprintf("  > XMEM test=%s speed=%s mode=%s : ", LFSR ? "LFSR" : "ADDR", speed_names[4], vram_mode_names[mode]);
+
+    // generate pattern_buffer data
+    if (LFSR)
+    {
+        fill_LFSR();
+    }
+    else
+    {
+        fill_ADDR();
+    }
+    RETURN_ON_KEYPRESS();
+
+    update_elapsed();
+    uint32_t start_time;
+    uint32_t check_time = elapsed_tenthms;
+    do
+    {
+        update_elapsed();
+        start_time = elapsed_tenthms;
+    } while (start_time == check_time);
+
+    // fill XMEM with pattern_buffer
+    {
+        // word tile mem
+        xm_setw(XR_ADDR, XR_TILE_ADDR);
+        for (int addr = XR_TILE_ADDR; addr < (XR_TILE_ADDR + XR_TILE_SIZE); addr++)
+        {
+            xm_setw(XR_DATA, pattern_buffer[addr]);
+        }
+        // word copper mem
+        xm_setw(XR_ADDR, XR_COPPER_ADDR);
+        for (int addr = XR_COPPER_ADDR; addr < (XR_COPPER_ADDR + XR_COPPER_SIZE); addr++)
+        {
+            xm_setw(XR_DATA, pattern_buffer[addr]);
+        }
+        // word color mem
+        xm_setw(XR_ADDR, XR_COLOR_A_ADDR);
+        for (int addr = XR_COLOR_A_ADDR; addr < (XR_COLOR_A_ADDR + XR_COLOR_A_SIZE + XR_COLOR_B_SIZE); addr++)
+        {
+            xm_setw(XR_DATA, pattern_buffer[addr]);
+        }
+    }
+
+    RETURN_ON_KEYPRESS();
+
+    read_xmem_buffer();
+    RETURN_ON_KEYPRESS();
+
+    // verify write was correct
+    xmem_errs += verify_xmem(LFSR, mode);
+    if (xmem_errs >= 16)
+    {
+        dprintf("TEST CANCELLED (too many errors)!\n");
+    }
+
+    if (xmem_errs == 0)
+    {
+        update_elapsed();
+        unsigned int elapsed_time = elapsed_tenthms - start_time;
+        dprintf("PASSED  (%3u.%1ums)\n", elapsed_time / 10, elapsed_time % 10);
+    }
+
+    return xmem_errs;
+}
+
 void xosera_test()
 {
     xv_prep();
@@ -653,7 +891,7 @@ void xosera_test()
 
         for (int i = 0; i < TEST_MODES; i++)
         {
-            for (int j = 0; j < TEST_SPEEDS; j++)
+            for (int j = 0; j < TEST_SPEEDS - 1; j++)
             {
                 test_vram(false, i, j);
                 if (delay_check(DELAY_TIME))
@@ -661,6 +899,18 @@ void xosera_test()
                     break;
                 }
                 test_vram(true, i, j);
+                if (delay_check(DELAY_TIME))
+                {
+                    break;
+                }
+            }
+            {
+                test_xmem(false, i);
+                if (delay_check(DELAY_TIME))
+                {
+                    break;
+                }
+                test_xmem(true, i);
                 if (delay_check(DELAY_TIME))
                 {
                     break;

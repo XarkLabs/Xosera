5523dbda85af61f31c4ea0b40b49c8bfb8abb77d
Xark <Xark@XarkLabs.com>
2022-05-15 14:51:14 -0700

    WIP borked

--- rtl/video_blend2.sv
+++ rtl/video_blend2.sv
@@ -33,64 +33,56 @@ module video_blend2#(
 logic           dv_de_1;            // display enable delayed
 logic           hsync_1;            // hsync delayed
 logic           vsync_1;            // vsync delayed
-rgb_t           blend_result;
+//rgb_t           blend_result;
 
-logic [7:0] rA;
-logic [7:0] gA;
-logic [7:0] bA;
+logic [7:0] alphaA;
+logic [7:0] inv_alphaA;
 
-logic [7:0] rB;
-logic [7:0] gB;
-logic [7:0] bB;
+logic [15:0] rA;
+logic [15:0] gA;
+logic [15:0] bA;
 
-logic [3:0] r_mix;
-logic [3:0] g_mix;
-logic [3:0] b_mix;
+logic [15:0] rB;
+logic [15:0] gB;
+logic [15:0] bB;
 
-always_comb rA      = { colorA_xrgb_i[11:8], colorA_xrgb_i[11:8] } * { 4'b0, ~colorB_xrgb_i[15:12] };
-always_comb gA      = { colorA_xrgb_i[ 7:4], colorA_xrgb_i[ 7:4] } * { 4'b0, ~colorB_xrgb_i[15:12] };
-always_comb bA      = { colorA_xrgb_i[ 3:0], colorA_xrgb_i[ 3:0] } * { 4'b0, ~colorB_xrgb_i[15:12] };
+// void blend4(uint8_t * result, uint8_t fg, uint8_t fa, uint8_t bg)
+// {
+//     unsigned int alpha     = fa + 1;
+//     unsigned int inv_alpha = 16 - fa;
+//     *result                = (uint8_t)((alpha * fg + inv_alpha * bg) >> 4);
+// }
 
-always_comb rB      = { colorB_xrgb_i[11:8], colorB_xrgb_i[11:8] } * { 4'b0, colorB_xrgb_i[15:12] };
-always_comb gB      = { colorB_xrgb_i[ 7:4], colorB_xrgb_i[ 7:4] } * { 4'b0, colorB_xrgb_i[15:12] };
-always_comb bB      = { colorB_xrgb_i[ 3:0], colorB_xrgb_i[ 3:0] } * { 4'b0, colorB_xrgb_i[15:12] };
+always_comb alphaA      = 8'h00;    //8'(colorB_xrgb_i[15:12]) + 8'h01;
+always_comb inv_alphaA  = 8'h10;    //8'h10 - 8'(colorB_xrgb_i[15:12]);
 
-always_comb r_mix   = rA[7:4] + rB[7:4];
-always_comb g_mix   = gA[7:4] + gB[7:4];
-always_comb b_mix   = bA[7:4] + bB[7:4];
+always_comb rA      = 8'(colorA_xrgb_i[11:8]) * alphaA;
+always_comb gA      = 8'(colorA_xrgb_i[ 7:4]) * alphaA;
+always_comb bA      = 8'(colorA_xrgb_i[ 3:0]) * alphaA;
+
+always_comb rB      = 8'(colorB_xrgb_i[11:8]) * inv_alphaA;
+always_comb gB      = 8'(colorB_xrgb_i[ 7:4]) * inv_alphaA;
+always_comb bB      = 8'(colorB_xrgb_i[ 3:0]) * inv_alphaA;
 
 logic unused_bits;
-always_comb unused_bits = &{ 1'b0, colorA_xrgb_i[14:12], colorB_xrgb_i[13:12], rA[3:0], gA[3:0], bA[3:0], rB[3:0], gB[3:0], bB[3:0], EN_BLEND_ADDCLAMP ? 1'b0 : 1'b0 };
+always_comb unused_bits = &{ 1'b0, colorA_xrgb_i, colorB_xrgb_i, rA, gA, bA, rB, gB, bB, EN_BLEND_ADDCLAMP || EN_BLEND ? 1'b0 : 1'b0 };
 
-generate
-    if (EN_BLEND) begin : opt_BLEND
-        always_comb begin
-            case (colorA_xrgb_i[15])
-                1'b0:          // A alpha 00xx = Use 2-bit alpha B blend value for alpha blend
-                    // Conceptually, for alpha purposes A is the bottom "destination" surface,
-                    // and B is "source" playfield blended on top, over it.
-                    blend_result  = {   r_mix[3:0],
-                                        g_mix[3:0],
-                                        b_mix[3:0] };
-
-                1'b1:  // A alpha = 11XX = 100% A, ignore B (aka A on top)
-                    blend_result    = colorA_xrgb_i[11:0];
-            endcase
-        end
-    end else begin
-        assign blend_result = (~colorA_xrgb_i[15] & colorB_xrgb_i[15]) ? colorB_xrgb_i[11:0] : colorA_xrgb_i[11:0];
-    end
-endgenerate
+//generate
+//    if (EN_BLEND) begin : opt_BLEND
+        // always_comb begin
+        //     // Conceptually, for alpha purposes A is the bottom "destination" surface,
+        //     // and B is "source" playfield blended on top, over it.
+        //     blend_result  = {   rA[3:0] + rB[3:0],
+        //                         gA[3:0] + gB[3:0],
+        //                         bA[3:0] + bB[3:0] };
+        // end
+    // end else begin
+    //     assign blend_result = (~colorA_xrgb_i[15] & colorB_xrgb_i[15]) ? colorB_xrgb_i[11:0] : colorA_xrgb_i[11:0];
+    // end
+//endgenerate
 
 // color RAM lookup (delays video 1 cycle for BRAM)
 always_ff @(posedge clk) begin
-
-    // color lookup happened on dv_de cycle
-    if (dv_de_1) begin
-        blend_rgb_o <= blend_result;
-    end else begin
-        blend_rgb_o <= '0;
-    end
 
     // delay signals for color lookup
     vsync_1     <= vsync_i;
@@ -101,6 +93,16 @@ always_ff @(posedge clk) begin
     dv_de_o     <= dv_de_1;
     vsync_o     <= vsync_1;
     hsync_o     <= hsync_1;
+
+    // color lookup happened on dv_de cycle
+    if (dv_de_1) begin
+        blend_rgb_o <= {    rA[3:0] + rB[3:0],
+                            gA[3:0] + gB[3:0],
+                            bA[3:0] + bB[3:0] };
+    end else begin
+        blend_rgb_o <= '0;
+    end
+
 end
 
 endmodule

56c4e1a7f0e51def0313ac6227bbc4b449f91232
Xark <Xark@XarkLabs.com>
2023-08-27 20:12:34 -0700

    last pixel bug, ~20% faster linedraw, X cursor

--- xosera_pointer_m68k/pointer_test_m68k.c
+++ xosera_pointer_m68k/pointer_test_m68k.c
@@ -151,7 +151,10 @@ static bool load_sd_colors(const char * filename)
     }
 }
 
-void line_draw(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
+// original version translated from Pascal
+// from https://ia801900.us.archive.org/0/items/byte-magazine-1988-03/byte-magazine-1988-03.pdf
+// "Better Bit-mapped Lines" pg. 249
+void line_draw_orig(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
 {
     int16_t x = x1, y = y1;
     int16_t d;
@@ -201,7 +204,7 @@ void line_draw(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
     inc_nondiag = b + b;
     inc_diag    = b + b - a - a;
 
-    for (int16_t i = 0; i < a; i++)
+    for (int16_t i = 0; i <= a; i++)
     {
         xm_setw(PIXEL_X, x);
         xm_setw(PIXEL_Y, y);
@@ -222,21 +225,184 @@ void line_draw(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
     }
 }
 
+// slightly optimized version ~20% faster
+void line_draw(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
+{
+    int16_t x = x1, y = y1;
+    int16_t d;
+    int16_t a = x2 - x1, b = y2 - y1;
+    int16_t dx_diag, dy_diag;
+    int16_t dx_nondiag, dy_nondiag;
+    int16_t inc_nondiag, inc_diag;
+
+    xv_prep();
+
+    // always draw first pixel, and this sets up full data word and latch
+    xm_setw(PIXEL_X, x);
+    xm_setw(PIXEL_Y, y);
+    xm_setw(DATA, color);
+
+    if (a < 0)
+    {
+        a       = -a;
+        dx_diag = -1;
+    }
+    else
+    {
+        dx_diag = 1;
+    }
+
+    if (b < 0)
+    {
+        b       = -b;
+        dy_diag = -1;
+    }
+    else
+    {
+        dy_diag = 1;
+    }
+
+    // instead of swapping for one loop, have x major and y major loops
+    // remove known zero terms
+    if (a < b)
+    {
+        dy_nondiag = dy_diag;
+
+        d           = a + a - b;
+        inc_nondiag = a + a;
+        inc_diag    = a + a - b - b;
+
+        // start count at one (drew one pixel already )
+        for (int16_t i = 1; i <= b; i++)
+        {
+            if (d < 0)
+            {
+                y += dy_nondiag;
+                d += inc_nondiag;
+                // x not changing, don't need to set it
+                xm_setw(PIXEL_Y, y);
+                xm_setbl(DATA, color);        // we can get away with bl here, since upper byte is latched
+            }
+            else
+            {
+                x += dx_diag;
+                y += dy_diag;
+                d += inc_diag;
+                xm_setw(PIXEL_X, x);
+                xm_setw(PIXEL_Y, y);
+                xm_setbl(DATA, color);        // we can get away with bl here, since upper byte is latched
+            }
+        }
+    }
+    else
+    {
+        dx_nondiag = dx_diag;
+
+        d           = b + b - a;
+        inc_nondiag = b + b;
+        inc_diag    = b + b - a - a;
+
+        // start count at one (drew one pixel already )
+        for (int16_t i = 1; i <= a; i++)
+        {
+            if (d < 0)
+            {
+                x += dx_nondiag;
+                d += inc_nondiag;
+                // y not changing, don't need to set it
+                xm_setw(PIXEL_X, x);
+                xm_setbl(DATA, color);        // we can get away with bl here, since upper byte is latched
+            }
+            else
+            {
+                x += dx_diag;
+                y += dy_diag;
+                d += inc_diag;
+                xm_setw(PIXEL_X, x);
+                xm_setw(PIXEL_Y, y);
+                xm_setbl(DATA, color);        // we can get away with bl here, since upper byte is latched
+            }
+        }
+    }
+}
+
+void line_test_orig()
+{
+    uint16_t color = 0;
+
+    xv_prep();
+
+    uint16_t start_time;
+    uint16_t check_time = xm_getw(TIMER);
+    do
+    {
+        start_time = xm_getw(TIMER);
+    } while (start_time == check_time);
+
+    for (int16_t x = 0; x < 320; x += 3)
+    {
+        line_draw_orig(x, 0, 319 - x, 239, color);
+        color = (color == 0xffff) ? 0 : color + 0x1111;
+    }
+    for (int16_t y = 0; y < 240; y += 3)
+    {
+        line_draw_orig(0, y, 319, 239 - y, color);
+        color = (color == 0xffff) ? 0 : color + 0x1111;
+    }
+
+    uint16_t elapsed_time = xm_getw(TIMER) - start_time;
+    dprintf("line_test original  (%3u.%1ums)\n", elapsed_time / 10, elapsed_time % 10);
+}
+
+
 void line_test()
 {
     uint16_t color = 0;
 
-    for (int16_t x = 0; x < 320; x += 7)
+    xv_prep();
+
+    uint16_t start_time;
+    uint16_t check_time = xm_getw(TIMER);
+    do
+    {
+        start_time = xm_getw(TIMER);
+    } while (start_time == check_time);
+
+    for (int16_t x = 0; x < 320; x += 3)
     {
         line_draw(x, 0, 319 - x, 239, color);
         color = (color == 0xffff) ? 0 : color + 0x1111;
     }
-    for (int16_t y = 0; y < 240; y += 7)
+    for (int16_t y = 0; y < 240; y += 3)
     {
         line_draw(0, y, 319, 239 - y, color);
         color = (color == 0xffff) ? 0 : color + 0x1111;
     }
+
+    uint16_t elapsed_time = xm_getw(TIMER) - start_time;
+    dprintf("line_test optimized (%3u.%1ums)\n", elapsed_time / 10, elapsed_time % 10);
 }
+
+uint16_t eks[256] = {
+    0xf100, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f, 0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x0000, 0x01f1, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x001f, 0x1000, 0x0000, 0x0000,
+    0x0000, 0x0000, 0x0001, 0xf100, 0x0001, 0xf100, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f, 0x1000, 0x0000, 0x1f10,
+    0x0000, 0x0000, 0x0000, 0x0000, 0x01f1, 0x0000, 0x0000, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x0000,
+    0x0000, 0x001f, 0x1000, 0x0000, 0x0000, 0x0001, 0xf100, 0x0000, 0x0000, 0x0001, 0xf100, 0x0000, 0x0000, 0x001f,
+    0x1000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x0000, 0x0000, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x01f1, 0x0000,
+    0x0000, 0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f, 0x1000, 0x0001, 0xf100, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x0001, 0xf100, 0x001f, 0x1000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x01f1, 0x0000, 0x0000, 0x0000,
+    0x0000, 0x0000, 0x0000, 0x01f1, 0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x01f1,
+    0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x0001, 0xf100, 0x001f, 0x1000, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f, 0x1000, 0x0001, 0xf100, 0x0000, 0x0000,
+    0x0000, 0x0000, 0x01f1, 0x0000, 0x0000, 0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x0000, 0x0000, 0x01f1,
+    0x0000, 0x0000, 0x0000, 0x0001, 0xf100, 0x0000, 0x0000, 0x001f, 0x1000, 0x0000, 0x0000, 0x001f, 0x1000, 0x0000,
+    0x0000, 0x0001, 0xf100, 0x0000, 0x0000, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x1f10, 0x0000, 0x0000, 0x1f10,
+    0x0000, 0x0000, 0x0000, 0x0000, 0x01f1, 0x0000, 0x0001, 0xf100, 0x0000, 0x0000, 0x0000, 0x0000, 0x001f, 0x1000,
+    0x001f, 0x1000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0001, 0xf100, 0x01f1, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+    0x0000, 0x1f10, 0x1f10, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x01f1, 0xf100, 0x0000, 0x0000, 0x0000,
+    0x0000, 0x0000, 0x0000, 0x001f};
 
 uint32_t test_count;
 void     xosera_pointer_test()
@@ -305,6 +471,14 @@ void     xosera_pointer_test()
     }
 
 #if 1
+    // X for testing
+    xmem_set_addr(XR_POINTER_ADDR);
+    for (uint16_t v = 0; v < XR_POINTER_SIZE; v++)
+    {
+        xmem_setw_next(eks[v]);
+    }
+
+#elif 0
     // set corners of default pointer for testing
     xmem_setw(XR_POINTER_ADDR + 7, 0x000f);
     xmem_setw(XR_POINTER_ADDR + 31 * 8, 0xf000);
@@ -351,7 +525,17 @@ void     xosera_pointer_test()
     xm_setw(PIXEL_Y, 320 / 4);        // words per line
     xm_setbh(FEATURE, 0x00);          // init pixel address generator
 
+    dprintf("\nLine benchmark:\n");
+    line_test_orig();
+    cpu_delay(3000);
     line_test();
+
+    dprintf("\n\nFor cheesy painting:\n\n");
+    dprintf("    A / Z  move pointer up / down\n");
+    dprintf("    , / .  move pointer left / right\n");
+    dprintf("    SPACE  cycles color\n");
+    dprintf("    r      resets to center\n");
+    dprintf("    ESC    exit (or Kermit)\n");
 
     while (!done)
     {
@@ -392,7 +576,7 @@ void     xosera_pointer_test()
         }
 
         wait_vblank_start();
-        xosera_set_pointer(px, py, 0xF000);
+        xosera_set_pointer(px - 15, py - 15, 0xF000);
     }
 
     xreg_setw(PA_GFX_CTRL, 0x0000);        // un-blank screen

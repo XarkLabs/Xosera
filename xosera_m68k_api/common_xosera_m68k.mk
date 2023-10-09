2acfac9b30e98671b8b54b78d03e145af0856667
Xark <Xark@XarkLabs.com>
2023-10-09 13:30:55 -0700

    WIP makefile tweaks

--- xosera_m68k_api/common_xosera_m68k.mk
+++ xosera_m68k_api/common_xosera_m68k.mk
@@ -18,7 +18,7 @@ $(info No m68k-elf-* build tools found in path)
 else
 # check for rosco_m68k build dir
 ifndef ROSCO_M68K_DIR
-$(info Please set ROSCO_M68K_DIR to the rosco_m68k directory to use for rosco_m68k building)
+$(info Please set ROSCO_M68K_DIR to the rosco_m68k root directory to use for rosco_m68k building)
 endif
 endif
 # check for xosera_m68k_api build dir
@@ -26,28 +26,33 @@ ifndef XOSERA_M68K_API
 $(info Please set XOSERA_M68K_API to the xosera_m68k_api directory to use for Xosera m68k building)
 endif
 
-# include special user rosco_m68k build riles
 -include $(ROSCO_M68K_DIR)/user.mk
 
-CPU?=68010
-EXTRA_CFLAGS?=-g -O2 -fomit-frame-pointer -I$(XOSERA_M68K_API)
+EXTRA_CFLAGS?=-I$(XOSERA_M68K_API)
 EXTRA_VASMFLAGS?=
 EXTRA_LIBS?=-L$(XOSERA_M68K_API) -lxosera_m68k_api
+
+CPU?=68010
+ARCH?=$(CPU)
+TUNE?=$(CPU)
+
 SYSINCDIR?=$(ROSCO_M68K_DIR)/code/software/libs/build/include
 SYSLIBDIR?=$(ROSCO_M68K_DIR)/code/software/libs/build/lib
+
 DEFINES=-DROSCO_M68K
-CFLAGS=-ffreestanding -ffunction-sections -fdata-sections \
-    -Wall -Wextra -Werror -Wno-unused-function -pedantic -I$(SYSINCDIR) \
-    -mcpu=$(CPU) -march=$(CPU) -mtune=$(CPU) $(DEFINES)
+FLAGS=-ffreestanding -ffunction-sections -fdata-sections -fomit-frame-pointer	\
+      -Wall -Wextra -Werror -Wno-unused-function -pedantic -I$(SYSINCDIR)			\
+      -mcpu=$(CPU) -march=$(ARCH) -mtune=$(TUNE) -g -O2 $(DEFINES)
+CFLAGS=-std=c11 $(FLAGS)
 # EVIL    	CFLAGS += -Wall -Wextra -Wpedantic -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2 -Wformat-security -Wnull-dereference -Wstack-protector -Wtrampolines -Walloca -Wvla -Warray-bounds=2 -Wimplicit-fallthrough=3 -Wshift-overflow=2 -Wcast-qual -Wstringop-overflow=4 -Wconversion -Warith-conversion -Wlogical-op -Wduplicated-cond -Wduplicated-branches -Wformat-signedness -Wshadow -Wstrict-overflow=4 -Wswitch-default -Wswitch-enum -Wstack-usage=1000000 -Wcast-align=strict
 # Too-EVIL 	-Wundef -Wstrict-prototypes  -Wtraditional-conversion
 CXXFLAGS=-std=c++20 -fno-exceptions -fno-rtti $(FLAGS)
-GCC_LIBS=$(shell $(CC) --print-search-dirs \
-    | grep libraries:\ = \
-    | sed 's/libraries: =/-L/g' \
-    | sed 's/:/m68000\/ -L/g')m68000/
+GCC_LIBS?=$(shell $(CC) --print-search-dirs \
+          | grep libraries:\ = \
+          | sed 's/libraries: =/-L/g' \
+          | sed 's/:/m68000\/ -L/g')m68000/
 LIBS=$(EXTRA_LIBS) -lrosco_m68k -lgcc
-ASFLAGS=-mcpu=$(CPU) -march=$(CPU)
+ASFLAGS=-mcpu=$(CPU) -march=$(ARCH)
 
 ROSCO_M68K_HUGEROM?=
 ifneq ($(ROSCO_M68K_HUGEROM),false)
@@ -56,28 +61,62 @@ else
 LDSCRIPT?=$(SYSLIBDIR)/ld/serial/rosco_m68k_program.ld
 endif
 
-LDFLAGS=-T$(LDSCRIPT) -L$(SYSLIBDIR) \
-    -Map=$(MAP) --gc-sections --oformat=elf32-m68k
-VASMFLAGS=-Felf -m68010 -quiet -Lnf $(DEFINES)
+
+VASMFLAGS=-Felf -m$(CPU) -quiet -Lnf $(DEFINES)
+LDFLAGS=-T $(LDSCRIPT) -L $(SYSLIBDIR) -Map=$(MAP) --gc-sections --oformat=elf32-m68k
+
 CC=m68k-elf-gcc
-CPP=m68k-elf-gcc -E -xc
 CXX=m68k-elf-g++
 AS=m68k-elf-as
 LD=m68k-elf-ld
-NM=m68k-elf-nm
-LD=m68k-elf-ld
 AR=m68k-elf-ar
 RANLIB=m68k-elf-ranlib
+NM=m68k-elf-nm
+LD=m68k-elf-ld
 OBJDUMP=m68k-elf-objdump
 OBJCOPY=m68k-elf-objcopy
 SIZE=m68k-elf-size
 VASM=vasmm68k_mot
+CHMOD=chmod
+MKDIR=mkdir
+LSOF=lsof
 RM=rm -f
+CP=cp
 KERMIT=kermit
+SERIAL?=/dev/modem
+BAUD?=115200
+
 COPASM=$(XOSERA_M68K_API)/bin/copasm
 
-SERIAL?=/dev/modem
-BAUD?=9600
+# GCC-version-specific settings
+ifneq ($(findstring GCC,$(shell $(CC) --version 2>/dev/null)),)
+CC_VERSION:=$(shell $(CC) -dumpfullversion)
+CC_MAJOR:=$(firstword $(subst ., ,$(CC_VERSION)))
+# If this is GCC 12 or 13, add flag --param=min-pagesize=0 to CFLAGS
+ifeq ($(CC_MAJOR),12)
+CFLAGS+=--param=min-pagesize=0
+endif
+ifeq ($(CC_MAJOR),13)
+CFLAGS+=--param=min-pagesize=0
+endif
+endif
+
+# For systems without MMU support, aligning LOAD segments with pages is not needed
+# In those cases, provide fake page sizes to both save space and remove RWX warnings
+ifeq ($(CPU),68030)
+LD_LD_SUPPORT_MMU?=true
+endif
+ifeq ($(CPU),68040)
+LD_SUPPORT_MMU?=true
+endif
+ifeq ($(CPU),68060)
+LD_SUPPORT_MMU?=true
+endif
+LD_SUPPORT_MMU?=false
+ifneq ($(LD_SUPPORT_MMU),true)
+# Saves space in binaries, but will break MMU use
+LDFLAGS+=-z max-page-size=16 -z common-page-size=16
+endif
 
 # Output config (assume name of directory)
 PROGRAM_BASENAME=$(shell basename $(CURDIR))
@@ -90,18 +129,21 @@ MAP=$(PROGRAM_BASENAME).map
 SYM=$(PROGRAM_BASENAME).sym
 SYM_SIZE=$(PROGRAM_BASENAME)_size.sym
 
+CASMSOURCES=$(wildcard *.casm)
+CPASMSOURCES=$(wildcard *.cpasm)
+CASMOUTPUT=$(addsuffix .h,$(basename $(CASMSOURCES) $(CPASMSOURCES)))
 # Assume source files in Makefile directory are source files for project
-SOURCES+=$(wildcard *.c) $(wildcard *.cpp) $(wildcard *.S) $(wildcard *.asm) $(wildcard *.raw)
-
-# Assume copasm will generate header file with copper code
-CASMSOURCES?=
-CASMOUTPUT=$(CASMSOURCES:.casm=.h) $(CASMSOURCES:.cpasm=.h)
-
-# Assume copasm files will generate header files
-HEADERS+=$(wildcard *.h) $(addsuffix .h,$(basename $(wildcard *.casm))) $(addsuffix .h,$(basename $(wildcard *.cpasm)))
-
+CSOURCES=$(wildcard *.c)
+CXXSOURCES=$(wildcard *.cpp)
+CINCLUDES=$(wildcard *.h)
+SSOURCES=$(wildcard *.S)
+ASMSOURCES=$(wildcard *.asm)
+RAWSOURCES=$(wildcard *.raw)
+SOURCES+=$(CSOURCES) $(CXXSOURCES) $(SSOURCES) $(ASMSOURCES) $(RAWSOURCES)
 # Assume each source files makes an object file
 OBJECTS=$(addsuffix .o,$(basename $(SOURCES)))
+
+TO_CLEAN=$(OBJECTS) $(ELF) $(BINARY) $(MAP) $(SYM) $(SYM_SIZE) $(DISASM) $(addsuffix .casm.ii,$(basename $(CPASMSOURCES))) $(CASMOUTPUT) $(addsuffix .lst,$(basename $(SSOURCES) $(ASMSOURCES) $(CASMSOURCES)))
 
 all: $(BINARY) $(DISASM)
 
@@ -111,13 +153,13 @@ $(XOSERA_M68K_API)/libxosera_m68k_api.a:
 	@echo === Building Xosera m68k API...
 	cd $(XOSERA_M68K_API) && $(MAKE)
 endif
-
-$(ELF) : $(OBJECTS) $(XOSERA_M68K_API)/libxosera_m68k_api.a
+# $(XOSERA_M68K_API)/libxosera_m68k_api.a
+$(ELF) : $(OBJECTS)
 	$(LD) $(LDFLAGS) $(GCC_LIBS) $^ -o $@ $(LIBS)
 	$(NM) --numeric-sort $@ >$(SYM)
 	$(NM) --size-sort $@ >$(SYM_SIZE)
 	$(SIZE) $@
-	-chmod a-x $@
+	-$(CHMOD) a-x $@
 
 $(BINARY) : $(ELF)
 	$(OBJCOPY) -O binary $(ELF) $(BINARY)
@@ -125,39 +167,43 @@ $(BINARY) : $(ELF)
 $(DISASM) : $(ELF)
 	$(OBJDUMP) --disassemble -S $(ELF) >$(DISASM)
 
-$(OBJECTS): $(HEADERS) $(MAKEFILE_LIST)
+$(OBJECTS): $(MAKEFILE_LIST)
 
-%.h : %.casm
-	@mkdir -p $(@D)
-	$(COPASM) -v -l -i $(XOSERA_M68K_API) -o $@ $<
-
-%.h : %.cpasm
-	@mkdir -p $(@D)
-	$(CPP) -D__COPASM__=1 -I$(XOSERA_M68K_API) $< -o $(basename $<).casm.ii
-	$(COPASM) -v -l -i $(XOSERA_M68K_API) -o $@ $(basename $<).casm.ii
-
-%.o : %.c
-	@mkdir -p $(@D)
+%.o : %.c $(CINCLUDES)
+	@echo o from c CINCLUDES=$(CINCLUDES) CASMOUTPUT=$(CASMOUTPUT)
+	@$(MKDIR) -p $(@D)
 	$(CC) -c $(CFLAGS) $(EXTRA_CFLAGS) -o $@ $<
 
 %.o : %.cpp
-	@mkdir -p $(@D)
+	@echo o from cpp CINCLUDES=$(CINCLUDES) CASMOUTPUT=$(CASMOUTPUT)
+	@$(MKDIR) -p $(@D)
 	$(CXX) -c $(CXXFLAGS) $(EXTRA_CXXFLAGS) -o $@ $<
 
 %.o : %.asm
-	@mkdir -p $(@D)
+	@$(MKDIR) -p $(@D)
 	$(VASM) $(VASMFLAGS) $(EXTRA_VASMFLAGS) -L $(basename $@).lst -o $@ $<
+
+# CopAsm copper source
+%.h : %.casm
+	@echo h from casm CINCLUDES=$(CINCLUDES) CASMOUTPUT=$(CASMOUTPUT)
+	@$(MKDIR) -p $(@D)
+	$(COPASM) -v -l -i $(XOSERA_M68K_API) -o $@ $<
+
+# preprocessed CopAsm copper source
+%.h : %.cpasm
+	@echo h from cpasm CINCLUDES=$(CINCLUDES) CASMOUTPUT=$(CASMOUTPUT)
+	@$(MKDIR) -p $(@D)
+	$(CC) -E -xc -D__COPASM__=1 -I$(XOSERA_M68K_API) $< -o $(basename $<).casm.ii
+	$(COPASM) -v -l -i $(XOSERA_M68K_API) -o $@ $(basename $<).casm.ii
 
 # link raw binary file into executable (with symbols _binary_<name>_raw_start/*_end/*_size)
 %.o: %.raw
-	@mkdir -p $(@D)
-	$(OBJCOPY) -I binary -O elf32-m68k -B m68k:68000 $< $@
+	@$(MKDIR) -p $(@D)
+	$(OBJCOPY) -I binary -O elf32-m68k -B m68k:$(CPU) $< $@
 
 # remove targets that can be generated by this Makefile
 clean:
-	$(RM) $(OBJECTS) $(CASMOUTPUT) $(ELF) $(BINARY) $(MAP) $(SYM) $(SYM_SIZE) $(DISASM)
-	$(RM) $(addsuffix .lst,$(basename $(wildcard *.casm) $(wildcard *.cpasm) $(wildcard *.S) $(wildcard *.asm)))
-	$(RM) $(addsuffix .casm.ii,$(basename $(wildcard *.cpasm))) $(addsuffix .h,$(basename $(wildcard *.casm) $(wildcard *.cpasm)))
+	$(RM) $(TO_CLEAN)
 
 disasm: $(DISASM)
 
@@ -167,38 +213,38 @@ dump: $(BINARY)
 
 # upload binary to rosco (if ready and kermit present)
 load: $(BINARY)
-	-lsof -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
+	-$(LSOF) -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
 	$(KERMIT) -i -l $(SERIAL) -b $(BAUD) -s $(BINARY)
 
 # Linux (etc.) upload binary and connect with screen (free SERIAL port, kermit upload, open screen in shell window/tab)
-test: $(BINARY) $(DISASM)
-	-lsof -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
+linuxtest: $(BINARY) $(DISASM)
+	-$(LSOF) -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
 	$(KERMIT) -i -l $(SERIAL) -b $(BAUD) -s $(BINARY)
 	gnome-terminal --geometry=106x30 --title="rosco_m68k $(SERIAL)" -- screen $(SERIAL) $(BAUD)
 
 # Linux (etc.) connect with screen (free SERIAL port, opens screen in shell window/tab)
-term: $(BINARY) $(DISASM)
-	-lsof -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
+linuxterm: $(BINARY) $(DISASM)
+	-$(LSOF) -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
 	gnome-terminal --geometry=106x30 --title="rosco_m68k $(SERIAL)" -- screen $(SERIAL) $(BAUD)
 
 # macOS upload binary and connect with screen (free SERIAL port, kermit upload, open screen in shell window/tab)
 mactest: $(BINARY) $(DISASM)
-	-lsof -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
+	-$(LSOF) -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
 	$(KERMIT) -i -l $(SERIAL) -b $(BAUD) -s $(BINARY)
 	echo "#! /bin/sh" > $(TMPDIR)/rosco_screen.sh
 	echo "/usr/bin/screen $(SERIAL) $(BAUD)" >> $(TMPDIR)/rosco_screen.sh
-	chmod +x $(TMPDIR)/rosco_screen.sh
+	-$(CHMOD) +x $(TMPDIR)/rosco_screen.sh
 	sleep 1
-	open -b com.apple.terminal $(TMPDIR)/rosco_screen.sh
+	open -n -b com.apple.terminal $(TMPDIR)/rosco_screen.sh
 
 # macOS connect with screen (free SERIAL port, open screen in shell window/tab)
 macterm:
-	-lsof -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
+	-$(LSOF) -t $(SERIAL) | (read oldscreen ; [ ! -z "$$oldscreen" ] && kill -3 $$oldscreen ; sleep 1)
 	echo "#! /bin/sh" > $(TMPDIR)/rosco_screen.sh
 	echo "/usr/bin/screen $(SERIAL) $(BAUD)" >> $(TMPDIR)/rosco_screen.sh
-	chmod +x $(TMPDIR)/rosco_screen.sh
+	-$(CHMOD) +x $(TMPDIR)/rosco_screen.sh
 	sleep 1
 	open -n -b com.apple.terminal $(TMPDIR)/rosco_screen.sh
 
 # Makefile magic (for "phony" targets that are not real files)
-.PHONY: all clean dump disasm load test term mactest macterm
+.PHONY: all clean disasm dump load linuxtest linuxterm mactest macterm
